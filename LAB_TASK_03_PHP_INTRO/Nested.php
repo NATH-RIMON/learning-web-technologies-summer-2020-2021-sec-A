afaa54af75afc2ff09df872cbfa5007f200dbab7
unknown <you@example.com>
2021-06-19 19:52:21 +0600

    task

--- LAB_TASK_03_PHP_INTRO/Nested.php
+++ LAB_TASK_03_PHP_INTRO/Nested.php
@@ -1,22 +1,14 @@
 <?php
-$a  = [
-    [1,2,3,"A"],
-    [1,2,"B","C"],
-    [1,"D","E","F"]
-];
-/*
-echo '<pre>';
-print_r($a);
-echo '<pre>';
-echo "<br>";*/
-for($i=0; $i <4 ;$i++){
+for($i=0;$i<4;$i++){
    
-    for($j=0;$j<4;$j++){
-        echo $a[$i][$j] ." ";
+    for($j=0;$j<$i;$j++){
+        echo "* ";
     }
     echo "<br>";
 }
 
+echo "second Pattern";
+echo "<br>";
 for($i=3;$i>=1;$i--){
    
     for($j=1;$j<=$i;$j++){
@@ -24,6 +16,9 @@ for($i=3;$i>=1;$i--){
     }
     echo "<br>";
 }
+
+echo "Third pattern";
+echo "<br>";
 $c=65;
 for($i=1;$i<=3;$i++){
    
@@ -33,4 +28,9 @@ for($i=1;$i<=3;$i++){
     }
     echo "<br>";
 }
+
+
+
+
+
 ?>
