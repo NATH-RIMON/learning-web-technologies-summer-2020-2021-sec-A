56a64e6ad3ea9968087270edf0d5f67c3af3929a
unknown <you@example.com>
2021-07-31 20:06:16 +0600

    ajex

--- Ajex/script.js
+++ Ajex/script.js
@@ -12,15 +12,31 @@ function ajax(){
 		}
 	}*/
 
-	let name = document.getElementById('name').value;
+	let email = document.getElementById('email').value;
 	const http = new XMLHttpRequest();
 	http.open('POST', `server.php`, true);
 	http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	http.send(`name=${name}`);
+	http.send(`email=${email}`);
 	http.onreadystatechange = function(){
 
 		if(this.readyState == 4 && this.status == 200){
 			document.getElementById('result').innerHTML = this.responseText;
+			
+		}
+	}
+} 
+function unsubscribe() {
+
+	let email = document.getElementById('email').value;
+	const http = new XMLHttpRequest();
+	http.open('POST', `unsubscribe.php`, true);
+	http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	http.send(`email=${email}`);
+	http.onreadystatechange = function(){
+
+		if(this.readyState == 4 && this.status == 200){
+			document.getElementById('result').innerHTML = this.responseText;
+			
 		}
 	}
 }
--- Ajex/server.php
+++ Ajex/server.php
@@ -1,11 +1,20 @@
 <?php
-	
-	$name = $_POST['name'];
 
-	if($name == null){
-		echo 'invalid data';
+	$email = $_POST['email'];
+
+	if($email == null){
+		echo 'invalid email';
 	}else{
-		echo "Valid data: ".$name;		
+		if(filter_var($email,FILTER_VALIDATE_EMAIL)==false)
+		{
+			echo 'invalid email';
+		}
+		else{
+		echo "Valid data: ".$email;	
+		$conn = mysqli_connect('localhost', 'root', '', 'test');
+		$sql = "insert into email_db values('','$email')"; 
+		$result = mysqli_query($conn, $sql);	
+		}
 	}
 
-?>
+?> 
